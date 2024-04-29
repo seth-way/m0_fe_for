da73302ab9e18b899cd6a3927726932e66c7def0
seth-way <seth.r.way@gmail.com>
2024-04-29 16:22:40 -0400

    complete solution for loop lesson

--- for.js
+++ for.js
@@ -1,22 +1,39 @@
 // As you complete each challenge, RUN THE CODE to ensure it works. You can do this by
 // running `node <filepath>`. If you are in this directory, you'd run `node for.js`.
-  
+
 // Challenge 1
+console.log('CHALLENGE 1:');
 // Use a for statement to log to the console all even numbers between 25 and 75.
 
-
+for (var i = 25; i < 76; i += 1) {
+  if (i % 2 === 0) console.log(i);
+}
 
 // Challenge 2
+console.log('\nCHALLENGE 2:');
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
-var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+var weather_options = [
+  'sunny',
+  'snowy',
+  'freezing',
+  'partly cloudy',
+  'drizzly',
+];
 
+for (var i = 0; i < weather_options.length; i += 1) {
+  console.log('Today, the weather is ' + weather_options[i]);
+}
 
 // Challenge 3
+console.log('\nCHALLENGE 3:');
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
-var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+var words = ['car', 'bike', 'bus', 'van', 'scooter', 'truck', 'rv'];
 
-// 📹 Record yourself live-coding and talking through any ONE of the challenges. 
-  // The recording doesn't have to be the first time you are solving the problem. 
-  // Use technical vocabulary and explain each part to demonstrate your understanding! 
-  // Share the link to your video in your small group channel.
+for (var i = 0; i < words.length; i += 1) {
+  if (words[i].length === 3) console.log(words[i]);
+}
 
+// 📹 Record yourself live-coding and talking through any ONE of the challenges.
+// The recording doesn't have to be the first time you are solving the problem.
+// Use technical vocabulary and explain each part to demonstrate your understanding!
+// Share the link to your video in your small group channel.
